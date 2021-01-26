456c8fce2a4dd444e4cf9130cd50fba5e2ce2f14
Jordan Gardner <jordanthomasg@gmail.com>
2021-01-26 07:10:34 -0700

    Add options
    Fix #1 by adding options to the naruto function for customizing duration
    and size.

--- README.md
+++ README.md
@@ -15,12 +15,28 @@ import naruto from 'animexyz'
 naruto()
 ```
 
+## Options
+
+``naruto`` accepts an optional object as its only parameter that allows
+you to override the default settings.  Options include:
+
+| Key     | Type | Default | Description                             |
+|---------|------|---------|-----------------------------------------|
+|duration |int   |500      |duration of the animation (milliseconds) |
+|size     |int   |50       |size of the Naruto image (pixels)        |
+
+```js
+// make Naruto even faster!
+naruto({duration: 200})
+```
+
 ## react-router-dom integration
 
 To see Naruto on every page change, subscribe to location changes:
 
 ```js
 import naruto from 'animexyz'
+import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 export function YourComponent() {
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-"use strict";
+'use strict'
 
 require('./index.css')
 
-module.exports = function() {
-    const container = document.createElement('div')
-    container.id = 'animexyz-naruto'
-    const img = document.createElement('img')
-    img.src = 'https://raw.githubusercontent.com/ngoue/animeXYZ/main/naruto.gif'
-    container.appendChild(img)
-    document.body.appendChild(container)
-    setTimeout(() => {
-      container.remove()
-    }, 500)
+// hosted courtesy of github
+const narutoSrc = `https://raw.githubusercontent.com/ngoue/animeXYZ/main/naruto.gif`
+
+module.exports = function (options) {
+  // get options (defaults set here)
+  const {duration, size} = Object.assign({
+    duration: 500,
+    size: 50,
+  }, options)
+
+  // create and style components
+  const container = document.createElement('div')
+  container.id = `animexyz-naruto`
+  container.style.left = `-${size}px`
+  container.style.animationDuration = `${(duration/1000)}s`
+
+  const img = document.createElement(`img`)
+  img.src = narutoSrc
+  img.style.height = `${size}px`
+
+  // add elements to the DOM
+  container.appendChild(img)
+  document.body.appendChild(container)
+
+  // remove after animating
+  setTimeout(() => {
+    container.remove()
+  }, duration)
 }
